a9a50626ff40c7d389e06a72b2d7d1f262a21333
Pedro Luís Azevedo Costa <pdcost@icloud.com>
2023-11-20 21:26:34 +0000

    Ajusntando optainet sens para ter um timeout e reduzindo o número de geraçoes

--- sensitiviy_analysis/sens_optainet.py
+++ sensitiviy_analysis/sens_optainet.py
@@ -1,3 +1,4 @@
+
 import sys
 sys.path.append('../')
 from optm_algorithms.pso import PSO
@@ -11,20 +12,38 @@ import warnings
 from tqdm import tqdm
 import json
 
+import threading
+import time
+
+
 rocket_fitness = RocketFitness(bound_values, num_workers=4)
 random_values = np.random.rand(10,10)
 fitness_func_class = rocket_fitness.calc_fitness
 
 nc_list = np.array([2, 4, 8])
 beta_list = np.array([10,100,1000])
-clone_threshold_list = np.linspace(0.05, 0.15, 3)
-supression_threshold_list = np.linspace(0.05, 0.5, 4)
-newcomers_percentage_list = np.linspace(0.1, 0.5, 5)
+clone_threshold_list = np.array([0.05, 0.15, 3])
+supression_threshold_list = np.array([0.4, 0.8, 2])
+newcomers_percentage_list = np.array([0.1, 0.2, 0.4])
 
 grid1, grid2, grid3, grid4, grid5 = np.meshgrid(nc_list, beta_list, clone_threshold_list, supression_threshold_list, newcomers_percentage_list)
 combinations = np.vstack((grid1.ravel(), grid2.ravel(), grid3.ravel(), grid4.ravel(), grid5.ravel())).T
 
-print(combinations.shape)
+print(combinations)
+
+def run_optimization(opt_ai_net, best_solutions, time_limit):
+    def optimization_task():
+        best_solutions.extend(opt_ai_net.fit())
+
+    thread = threading.Thread(target=optimization_task)
+    thread.start()
+
+    thread.join(timeout=time_limit)
+    if thread.is_alive():
+        thread.join()  # Garantir que o thread termine antes de prosseguir
+        return False  # Indica que o timeout ocorreu
+    return True  # Indica que a execução foi concluída com sucesso
+
 
 def execute_sensitivity_analysis_optainet(combinations, filename):
     
@@ -38,8 +57,8 @@ def execute_sensitivity_analysis_optainet(combinations, filename):
         newcomers_percentage = row[4]
         
         opt_ai_net = OptAiNet( 
-                        num_epochs=100,
-                        pop_size=1000,
+                        num_epochs=50,
+                        pop_size=20,
                         Nc=nc,
                         chrom_length=10,
                         clone_threshold=clone_threshold,
@@ -50,23 +69,39 @@ def execute_sensitivity_analysis_optainet(combinations, filename):
                         fitness_func=fitness_func_class,
                         verbose=True,
                         eval_every=10,
-                        limit_fitness_calls= 1000 * 4 * 100
+                        limit_fitness_calls= 1000 * 2 * 100
         )
 
-        best_solutions = opt_ai_net.fit()
+        best_solutions = []
+        success = run_optimization(opt_ai_net, best_solutions, 1000)
 
-        dict_save = {'nc': nc,
-                    'beta': beta,
-                    'clone_threshold': clone_threshold,
-                    'supression_threshold': supression_threshold,
-                    'newcomers_percentage': newcomers_percentage,
-                    'fitness_calls': opt_ai_net.fitness_calls_list.tolist(),
-                    'best_ind_list': opt_ai_net.best_ind_list.tolist(),
-                    'avg_ind_list': opt_ai_net.avg_ind_list.tolist(),
-                    'best_solutions': best_solutions.tolist(),
-                    'total_time': opt_ai_net.total_exec_time,
-                    }
-        
+        if not success:  # Se o timeout ocorrer
+            dict_save = {
+                'nc': nc,
+                'beta': beta,
+                'clone_threshold': clone_threshold,
+                'supression_threshold': supression_threshold,
+                'newcomers_percentage': newcomers_percentage,
+                'fitness_calls': 0,
+                'best_ind_list': 0,
+                'avg_ind_list': 0,
+                'best_solutions': 0,
+                'total_time': 0,
+            }
+        else:
+            dict_save = {
+                'nc': nc,
+                'beta': beta,
+                'clone_threshold': clone_threshold,
+                'supression_threshold': supression_threshold,
+                'newcomers_percentage': newcomers_percentage,
+                'fitness_calls': opt_ai_net.fitness_calls_list.tolist(),
+                'best_ind_list': opt_ai_net.best_ind_list.tolist(),
+                'avg_ind_list': opt_ai_net.avg_ind_list.tolist(),
+                'best_solutions': best_solutions,
+                'total_time': opt_ai_net.total_exec_time,
+            }
+
         simulations_list.append(dict_save)
         
     with open(filename, 'w') as fout:
